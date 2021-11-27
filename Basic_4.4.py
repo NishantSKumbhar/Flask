7a43e08d2d9363c7540f1d1e90da06b81ca460e0
Nishant <nish.kumbhar02@gmail.com>
2021-11-27 22:11:30 +0530

    Create Basic_4.4.py

---
+++ Basic_4.4.py
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>contact</title>
+</head>
+<body>
+	<h1>This is the Contact Page.</h1>
+	<h1>Contact Here : </h1>
+	{{empl[0].ph_number}}<br>
+	{{empl[1].ph_number}}
+</body>
+</html>
