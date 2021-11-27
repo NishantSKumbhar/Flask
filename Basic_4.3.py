fe0054cb26fa9d8051072ed22fa97a289211c4ce
Nishant <nish.kumbhar02@gmail.com>
2021-11-27 22:11:08 +0530

    Create Basic_4.3.py

---
+++ Basic_4.3.py
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>about</title>
+</head>
+<body>
+	<h1>This is the About Page.</h1>
+	<h2>{{empl[0].name}}</h2>
+	{{empl[0].profile}}
+
+	<h2>{{empl[1].name}}</h2>
+	{{empl[1].profile}}
+</body>
+</html>
